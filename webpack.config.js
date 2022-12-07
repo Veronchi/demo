aa522af8f1b49b7bec72745509e58cc1066527cf
Veronchi <zhigunova1995@gmail.com>
2022-12-07 12:41:41 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/script.js",
+  output: {
+    filename: "./bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    hot: false,
+    liveReload: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Demo",
+      filename: "index.html",
+      template: "./src/templates/index.html",
+      inject: "body",
+      minify: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./images/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+};
