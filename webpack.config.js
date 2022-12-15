9b950afb32bcc3417623c3ef1f67d8385420dd22
Veronchi <zhigunova1995@gmail.com>
2022-12-15 15:51:31 +0300

    add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
           filename: "./images/[hash][ext][query]",
         },
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
